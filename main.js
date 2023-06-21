f42d1d2a566c0fe92a3176cbee87367d6c885c53
Walberson Dias da Silva <walberson.mv@gmail.com>
2023-06-21 20:15:39 -0300

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-import sigiloCartao from "./functions.js";
-console.log(sigiloCartao());
+/* import sigiloCartao from "./functions.js";
+console.log(sigiloCartao()); */
 /* import * as user from "./functions.js"; */
-
 
 /* let usuario = user.nome
 usuario = 'ciclano'
@@ -9,3 +8,23 @@ console.log(usuario)
  */
 /* const {usuario} = require('./functions.js')
 console.log(usuario) */
+
+const valores = [
+  {
+    nome: "ciclano",
+    idade: 20,
+  },
+  {
+    nome: "fulano",
+    idade: 30,
+  },
+  {
+    nome: "beltrano",
+    idade: 40,
+  },
+];
+
+const reduzido = valores.reduce((acumulador, current, index, array) => {
+  return acumulador + current.idade;
+}, 0);
+console.log(reduzido);
